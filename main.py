3ecccdbf9aec80d1ff7b0cde9344bcd6f307bb36
Fernanda Brandão <fernandabrandaojob@gmail.com>
2024-05-21 23:00:42 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("jornada de dados")
+
+#variáveis
+nome="Fernanda"
+idade= 28
+
+#print
+print(nome)
+
+#soma
+nova_idade=idade+1
+print(nova_idade)
+
+#lista
+lista=[1,2,3,4,5]
+
+#métodos
+lista_invertida=lista.reverse()
